f8f73fd1c00bb49433aa90b2180183e1691b2416
ahmaddewa <71802251+ahmaddewa@users.noreply.github.com>
2020-11-23 12:37:40 +0700

    Update footer.php

--- footer.php
+++ footer.php
@@ -2,7 +2,7 @@
 
 
   <div class="footer-copyright text-center py-3">&copy; 2020 Copyright:
-    <a href="https://mdbootstrap.com/"> Ahmad Dewa Fitrah</a>
+    <a href="lms.unigamalang.ac.id"> Ahmad Dewa Fitrah</a>
   </div>
  
 
